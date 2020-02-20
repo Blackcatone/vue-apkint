e02d4c85b2435aae864c446126b08746d895fe93
452292 <2805858091@qq.com>
2020-02-20 19:15:09 +0800

    包安装成功

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <FirstHome></FirstHome>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FirstHome from './components/Home.vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components:{
+    FirstHome
+    }
 }
 </script>
 
---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<!--  -->
+<template>
+<div id="firstcomponent">
+    <h1>首页</h1>
+    {{ msg }}
+</div>
+</template>
+
+<script>
+export default {
+    data () {
+    return {
+        msg: "首页面板"
+        }
+    },
+
+};
+</script>
+<style lang='scss' scoped>
+
+</style>
